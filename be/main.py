b113dd0991bfad3fadd33e5b636c5adeaefedd34
junho-baek <junho6610@yonsei.ac.kr>
2024-08-16 21:22:32 +0900

    feat: add dockerignore & update fast api server with model - base setting

--- be/main.py
+++ be/main.py
@@ -1,8 +1,9 @@
 # backend/main.py
 from fastapi import FastAPI
-
+import pandas as pd
 app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"Hello": "World"}
+    df = pd.read_json("top_5_songs.json")
+    return df
